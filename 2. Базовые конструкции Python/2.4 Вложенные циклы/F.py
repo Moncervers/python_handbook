d3b11955f59de2d80a4dc0a4b5fc4f7bf5c838e0
Savva Chernyshev <chesavva@gmail.com>
2024-05-21 21:07:18 +0300

    Initial commit

--- 2. Базовые конструкции Python/2.4 Вложенные циклы/F.py
+++ 2. Базовые конструкции Python/2.4 Вложенные циклы/F.py
@@ -2,13 +2,17 @@ count = int(input())
 res = 0
 num_list = []
 
-for i in range(count):
-    num_list.append(int(input()))
-#
-# while first_num != 0 and second_num != 0:
-#     if first_num > second_num:
-#         first_num = first_num % second_num
-#     else:
-#         second_num = second_num % first_num
-#
-# print(first_num + second_num)
+first_num = int(input())
+
+for i in range(count - 1):
+    second_num = int(input())
+
+    while first_num != 0 and second_num != 0:
+        if first_num > second_num:
+            first_num = first_num % second_num
+        else:
+            second_num = second_num % first_num
+
+    first_num = first_num + second_num
+
+print(first_num)
